20b7c04cfe4992db03d1d19518f9d984d1d6b3a4
312554OliverByl <47400006+312554OliverByl@users.noreply.github.com>
2019-06-18 12:11:00 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _312554CmdLineTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a command line program. Isn't it cool? (y/n)");
+            String answer = Console.ReadLine().Trim().ToLower();
+
+            if (answer == "y")
+                Console.WriteLine("Correct!");
+            else if (answer == "n")
+                Console.WriteLine("aw..");
+            else
+                Console.WriteLine("Ok then?");
+
+            //Wait for any input before ending program:
+            Console.ReadLine();
+        }
+    }
+}
